ca2985a95dc8d37c040b5ee1d65109140669739f
Peta0228 <peterwu0228@icloud.com>
2020-08-26 15:57:03 +0800

    js function to dispaly lang

--- js/custom_js.js
+++ js/custom_js.js
@@ -1,8 +1,155 @@
 function language_info(country) {
     document.getElementById('country').innerHTML = country
 
+    switch (country) {
+        case 'timor-leste':
+            document.getElementById('language').innerHTML = 'timor'
+            break;
+        case 'indonesia':
+            document.getElementById('language').innerHTML = 'indo'
+            break;
+        case 'singapore':
+            document.getElementById('language').innerHTML = 'sing'
+            break;
+        case 'brunei':
+            document.getElementById('language').innerHTML = 'bru'
+            break;
+        case 'malaysia':
+            document.getElementById('language').innerHTML = 'mal'
+            break;
+        case 'philippines':
+            document.getElementById('language').innerHTML = 'phil'
+            break;
+        case 'vietnam':
+            document.getElementById('language').innerHTML = 'viet'
+            break;
+        case 'cambodia':
+            document.getElementById('language').innerHTML = 'cam'
+            break;
+        case 'laos':
+            document.getElementById('language').innerHTML = 'lao'
+            break;
+        case 'thailand':
+            document.getElementById('language').innerHTML = 'thai'
+            break;
+        case 'myanmar':
+            document.getElementById('language').innerHTML = 'myan'
+            break;
+        case 'bangladesh':
+            document.getElementById('language').innerHTML = 'bang'
+            break;
+        case 'china':
+            document.getElementById('language').innerHTML = 'chi'
+            break;
+        case 'india':
+            document.getElementById('language').innerHTML = 'ind'
+            break;
+        case 'bhutan':
+            document.getElementById('language').innerHTML = 'bhu'
+            break;
+        case 'maldives':
+            document.getElementById('language').innerHTML = 'mal'
+            break;
+        case 'nepal':
+            document.getElementById('language').innerHTML = 'nep'
+            break;
+        case 'sri lanka':
+            document.getElementById('language').innerHTML = 'sri'
+            break;
+        case 'japan':
+            document.getElementById('language').innerHTML = 'jap'
+            break;
+        case 'south korea':
+            document.getElementById('language').innerHTML = 'sou'
+            break;
+        case 'north korea':
+            document.getElementById('language').innerHTML = 'nor'
+            break;
+        case 'mongolia':
+            document.getElementById('language').innerHTML = 'mon'
+            break;
+        case 'pakistan':
+            document.getElementById('language').innerHTML = 'pak'
+            break;
+        case 'afghanistan':
+            document.getElementById('language').innerHTML = 'afg'
+            break;
+        case 'tajikistan':
+            document.getElementById('language').innerHTML = 'taj'
+            break;
+        case 'kyrgyzstan':
+            document.getElementById('language').innerHTML = 'kyr'
+            break;
+        case 'uzbekistan':
+            document.getElementById('language').innerHTML = 'uzb'
+            break;
+        case 'turkmenistan':
+            document.getElementById('language').innerHTML = 'tur'
+            break;
+        case 'iran':
+            document.getElementById('language').innerHTML = 'ira'
+            break;
+        case 'kazakhstan':
+            document.getElementById('language').innerHTML = 'kaz'
+            break;
+        case 'yemen':
+            document.getElementById('language').innerHTML = 'yem'
+            break;
+        case 'oman':
+            document.getElementById('language').innerHTML = 'oma'
+            break;
+        case 'UAE':
+            document.getElementById('language').innerHTML = 'uae'
+            break;
+        case 'qatar':
+            document.getElementById('language').innerHTML = 'qat'
+            break;
+        case 'bahrain':
+            document.getElementById('language').innerHTML = 'bah'
+            break;
+        case 'saudi arabia':
+            document.getElementById('language').innerHTML = 'sau'
+            break;
+        case 'kuwait':
+            document.getElementById('language').innerHTML = 'kuw'
+            break;
+        case 'iraq':
+            document.getElementById('language').innerHTML = 'ira'
+            break;
+        case 'palestanian terr':
+            document.getElementById('language').innerHTML = 'pal'
+            break;
+        case 'jordan':
+            document.getElementById('language').innerHTML = 'jor'
+            break;
+        case 'isarel':
+            document.getElementById('language').innerHTML = 'isa'
+            break;
+        case 'lebanon':
+            document.getElementById('language').innerHTML = 'leb'
+            break;
+        case 'syria':
+            document.getElementById('language').innerHTML = 'syr'
+            break;
+        case 'turkey':
+            document.getElementById('language').innerHTML = 'tur'
+            break;
+        case 'armenia':
+            document.getElementById('language').innerHTML = 'arm'
+            break;
+        case 'georgia':
+            document.getElementById('language').innerHTML = 'geo'
+            break;
+        case 'azerbaijan':
+            document.getElementById('language').innerHTML = 'aze'
+            break;
+         case 'russia':
+            document.getElementById('language').innerHTML = 'rus'
+            break;
+        default:
+            break;
+    }
     
-    document.getElementById('language').innerHTML = 'aha'
     
 
 }
